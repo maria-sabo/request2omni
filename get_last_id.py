a061b981230a37ea76204d09099b6f876faeb9e0
maria <maria_sabo@rambler.ru>
2020-12-17 17:56:00 +0300

    comments added

--- get_last_id.py
+++ get_last_id.py
@@ -6,11 +6,15 @@ def get_last_id():
     :return:  Идентификатор заявки, либо False
     """
     file_name = 'request_ids.txt'
+    file = open(file_name, 'a+')
+    file.close()
     f_read = open(file_name, "r")
     num_lines = sum(1 for line in f_read)
     f_read = open(file_name, "r")
     if num_lines > 0:
         last_id = f_read.readlines()[-1]
+        f_read.close()
         return last_id.rstrip()
     else:
+        f_read.close()
         return False
